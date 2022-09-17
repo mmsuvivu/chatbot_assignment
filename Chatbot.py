44b54d87117d8534612a58ee5b4cf3f11e8ec89b
mmsuvivu <mmsuvivu@student.jyu.fi>
2022-09-17 12:41:55 +0200

    Refactored code and added dictionary for questions

--- Chatbot.py
+++ Chatbot.py
@@ -1,40 +1,21 @@
 
 from random import randrange
 import random
+import BotAnswers
+from BotQuestions import questions
+from TriggerWords import greetings, insults, happyWords, sadWords, angryWords, apologeticWords
+from BotAnswers import neutralGreetings, neutralQuestions, angryBotReactionArray
 
-#current state
+# Returns true if user answer was found in list
+def find(answer, list):
+    return any(item in answer.lower().strip() for item in list)
 
+
+# current state
 state = "neutral"
-#array of possible arrays
+
+# list of all states
 stateArray = ["neutral", "happy", "angry", "sad"]
-
-# These are greetings the chatbot can use when it's in a neutral state
-neutralGreetings = ["Hello. ", "Hi. ", "Nice to meet you. ", "Hey. "]
-
-neutralQuestions = ["How are you?", "How are you feeling?", "How is your day?"]
-
-
-# These are feelings the user can have if they are in a good mood
-goodUserAnswers = ["Good", "Well", "Fine", "Not bad", "Ok"]
-
-# These are feelings the user can have if they are in a sad mood
-sadUserAnswers = ["Horrible", "Terrible", "Bad", "Sad"]
-
-# These are feelings the user can have if they are in an angry mood
-angryUserAnswers = ["Why do you care?", "Shut up", "I'm not going to tell you", "You're dumb"]
-
-# Happy reactions the user may have to a happy chatbot
-happyUserReaction = ["That's nice!", "That makes me happy too!", "That is good", "thats nice"]
-
-# Angry reactions the user may have to a happy chatbot
-angryUserReaction = ["I don't care", "Why should I care?", "dumb robots can't have feelings", "You suck", "I don't like you"]
-
-# Apologetic reatcion the user may have
-apologeticReaction = ["I'm sorry", "I apologize", "Sorry for being rude", "My bad", "Excuse me please", "sorry"]
-
-# Random angry Reactions the bot may have to an angry User
-angryBotReactionArray = ["F you. ", "Go away. ", "Back off. ", "Apologise. ", "No. ", "I don't like you. ", "Log off. "]
-angryBotGreetingArray = ["No need to be rude. ", "I'm angry now. ", "Why would you say that?! ", "I can't believe you said that. "]
 
 # counter for how many angry reactions the bot gave
 angerCounter = 0
@@ -42,58 +23,67 @@ angerCounter = 0
 # counter for how many sad reactions the bot gave
 sadCounter = 0
 
-#
-sadBotGreetingArray = ["Why do you hate me? ", "I wanna be alone right now. ", "I feel like crying. ", "Please leave me alone. "]
+
+print("You are now talking with a chatbot! Say hi!")
+answer = input()  # I am asking the user to say something
+
+# If the user insults the robot, the robot becomes angry
+if find(answer, insults):
+    print("What a rude way to start a conversation!")
+    state = "angry"
+
+# If the user greets the robot with a neutral or friendly way, the robot becomes happy
+elif find(answer, greetings):
+    print(random.choice(neutralGreetings) + random.choice(neutralQuestions))
+
+# Otherwise the chatbot stays in a neutral state
+else:
+    print("Okayyy... Do you have any questions for me?")
 
 while 1 == 1:
-    if (state == "neutral"):
-        answer = input("Greet the robot: ") #I am asking the user to say something
-        if("hi" in answer or "hello" in answer or "hey" in answer):
-            print(random.choice(neutralGreetings) + random.choice(neutralQuestions))
+    answer = input()
+    # if the user asks the chatbot to shut down the program shuts down
+    if answer == "shut down":
+        print("Okay, shutting down")
+        break
 
-            answer = input()
-            
-            # if the user answers that they are good, the chatbot becomes happy
-            for user_answer in goodUserAnswers:
-                if user_answer.lower() in answer.lower():
-                    print("That's nice to hear!")
-                    state = "happy"
-                    
-        
+    foundQuestion = False
 
-            # if the user answers that they are not good, the chatbot becomes sad
-            for user_answer in sadUserAnswers:
-                if user_answer.lower() in answer.lower():
-                        answer = input("Oh no! What's wrong? ")
-                        state = "sad"
-                        print("That's really sad! I think I am sad now too.")
-                    
-                # if the user answers in an angry way, the chatbot becomes angry
-            for user_answer in angryUserAnswers:
-                if user_answer.lower() in answer.lower():
-                    print("There's no need to be rude.")
-                    state = "angry"
+    # if the user asks a question, the chatbot will answer it depending on the current state
+    for key in questions:
+        if key in answer:
+            print(questions[key][state])
+            foundQuestion = True
+            break
 
+    if foundQuestion:
+        continue
 
-    elif(state == "happy"):
+    # if the user apologises when the chatbot is angry, it becomes neutral
+    if state == "angry":
+        if find(answer, apologeticWords):
+            print("It's okay")
+            state = "neutral"
+        else:
+            print(random.choice(angryBotReactionArray))
+    # TODO: elif state == "sad" implement the sad counter system and print sad answers randomly
+    else:
+        if find(answer, angryWords):
+            state = "angry"
+            print(random.choice(angryBotReactionArray))
+        elif find(answer, sadWords):
+            state = "sad"
+            print("Oh no") # TODO: This needs to be a list of different answers (just like random.choice(angryBotReactionArray))
+        elif find(answer, happyWords):
+            state = "happy"
+            print("That's nice") # TODO: This needs to be a list of different answers (just like random.choice(angryBotReactionArray))
+        else:
+            state = "neutral"
+            print("I don't understand") # TODO: This needs to be a list of different answers (just like random.choice(angryBotReactionArray))
 
-        answer = input("I am happy now! ")
-        while state == "happy":
-                state = random.choice(stateArray)
-
-        # if the user reaction is good the robot goes into a random mood
-        for user_answer in happyUserReaction:
-                if user_answer.lower() in answer.lower():
-                    print("Thank you!")
-
-        # if the user answers in an angry way, the chatbot becomes angry
-        for user_answer in angryUserAnswers:
-                if user_answer.lower() in answer.lower():
-                    print("There's no need to be rude.")
-                    state = "angry"
-                    
-
-
+    # TODO: when the code below this comment is implemented to the code above, this old code can be deleted
+    """
+    
     elif(state == "angry"):
         print(random.choice(angryBotGreetingArray))
         answer = input("Say something, stupid: ")
@@ -133,5 +123,6 @@ while 1 == 1:
                         print("I have calmed down now")
                         while state == "sad":
                                 state = random.choice(stateArray)
-                        
-                        
+    """
+
+
